b23cba4302c6f72d17e31625f388a8de1fd8845d
Goorac <82958803+Goorac@users.noreply.github.com>
2021-06-09 16:45:36 +0530

    Add files via upload

---
+++ src/js/questions.js
@@ -0,0 +1,74 @@
+//Topic name
+document.getElementById("topic").innerHTML = "Trees and leafs"
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Trees are leafless for a shorter or longer season of the year in",
+    answer: "Scrub Jungle forest",
+    options: [
+      "Evergreen forest",
+      "Mangrove forest",
+      "Scrub Jungle forest",
+      "Deciduous forest"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Oxygen liberated during photosynthesis is coming from",
+    answer: "Water",
+    options: [
+      "Carbon dioxide",
+      "Water",
+      "Break down of chlorophyll",
+      "Atmosphere"
+    ]
+  },
+    {
+    numb: 3,
+    question: " Stilt roots are found in",
+    answer: "Maize",
+    options: [
+      "Banyan",
+      "Maize",
+      "Mango",
+      "China rose"
+    ]
+  },
+    {
+    numb: 4,
+    question: " Fruits of this plant are found underground -",
+    answer: "Groundnut",
+    options: [
+      "Potato",
+      "Carrot",
+      "Groundnut",
+      "Onion"
+    ]
+  },
+    {
+    numb: 5,
+    question: " Which of the following is not a root?",
+    answer: "Potato",
+    options: [
+      "Potato",
+      "Carrot",
+      "Radish",
+      "Turnip"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
