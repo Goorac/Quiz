d01ea6def38086806c159d3987073a9a52da558f
Goorac <82958803+Goorac@users.noreply.github.com>
2021-06-09 21:42:22 +0530

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -195,11 +195,11 @@ function startTimer(time){
 }
 
 function startTimerLine(time){
-    counterLine = setInterval(timer, 58);
+    counterLine = setInterval(timer, 88);
     function timer(){
         time += 1; //upgrading time value with 1
         time_line.style.width = time + "px"; //increasing width of time_line with px by time value
-        if(time > 549){ //if time value is greater than 549
+        if(time > 330){ //if time value is greater than 549
             clearInterval(counterLine); //clear counterLine
         }
     }
