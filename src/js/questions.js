f43c7da75fcc79544ff47aee543e96d0c9a8fecd
Goorac <82958803+Goorac@users.noreply.github.com>
2021-06-10 15:46:11 +0530

    Add files via upload

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,60 +1,60 @@
 //Topic name
-document.getElementById("topic").innerHTML = "Trees and leafs"
+document.getElementById("topic").innerHTML = "Skeletal System"
 // creating an array and passing the number, questions, options, and answers
 let questions = [
     {
     numb: 1,
-    question: "Trees are leafless for a shorter or longer season of the year in",
-    answer: "Scrub Jungle forest",
+    question: "Number of vertebrae in human is",
+    answer: "26",
     options: [
-      "Evergreen forest",
-      "Mangrove forest",
-      "Scrub Jungle forest",
-      "Deciduous forest"
+      "33",
+      "32",
+      "22",
+      "26"
     ]
   },
     {
     numb: 2,
-    question: "Oxygen liberated during photosynthesis is coming from",
-    answer: "Water",
+    question: "The longest bone in the human body is",
+    answer: "humerus",
     options: [
-      "Carbon dioxide",
-      "Water",
-      "Break down of chlorophyll",
-      "Atmosphere"
+      "stapes",
+      "humerus",
+      "ulna",
+      "humerus"
     ]
   },
     {
     numb: 3,
-    question: " Stilt roots are found in",
-    answer: "Maize",
+    question: "How many bones are present in human skull?",
+    answer: "22",
     options: [
-      "Banyan",
-      "Maize",
-      "Mango",
-      "China rose"
+      "42",
+      "32",
+      "12",
+      "22"
     ]
   },
     {
     numb: 4,
-    question: " Fruits of this plant are found underground -",
-    answer: "Groundnut",
+    question: "The total number of facial bones is",
+    answer: "14",
     options: [
-      "Potato",
-      "Carrot",
-      "Groundnut",
-      "Onion"
+      "12",
+      "16",
+      "14",
+      "18"
     ]
   },
     {
     numb: 5,
-    question: " Which of the following is not a root?",
-    answer: "Potato",
+    question: "Which of the following is not associated with the bones in human body?",
+    answer: "Control of blood sugar",
     options: [
-      "Potato",
-      "Carrot",
-      "Radish",
-      "Turnip"
+      "Production of red blood cells",
+      "Control of blood sugar",
+      "Protection of internal organs",
+      "Storage of minerals"
     ]
   },
   // you can uncomment the below codes and make duplicate as more as you want to add question
